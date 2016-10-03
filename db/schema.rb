86ac5c75bd9fd3077c7ecfae01f4592df9824360
mrogach2350 <m.rogach@gmail.com>
2016-10-03 11:30:06 -0700

    adds address details migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161002193352) do
+ActiveRecord::Schema.define(version: 20161003182645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.date     "birthdate"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string   "name"
+    t.float    "quantity"
+    t.string   "brand"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string   "name"
@@ -25,10 +42,14 @@ ActiveRecord::Schema.define(version: 20161002193352) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.integer  "phone"
-    t.string   "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "phone"
+    t.string   "street_address"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "state"
+    t.integer  "zip"
+    t.string   "country"
+    t.string   "street_address2"
   end
 
 end
